567d0da6cca96e579b8392a96276404958d2eb8f
Rowad <rowadwo@gmail.com>
2020-03-31 09:45:56 +0300

    vid 5 many to one self refd

--- src/index.js
+++ src/index.js
@@ -12,7 +12,18 @@ sequelize
     // await Posts.sync({ force: true });
     // await Types.sync({ force: true });
     // await UsersTypes.sync({ force: true });
-    sequelize.sync({ force: true });
+    await sequelize.sync({ force: true });
+    const res = await Users.findAll({
+      include: ['supervisor', 'employees'],
+      logging: console.log
+    });
+    console.log(
+      JSON.stringify(
+        res.map(el => el.get({ plain: true })),
+        null,
+        2
+      )
+    );
   })
   .catch(err => {
     console.error(err);
@@ -26,6 +37,10 @@ const defineRelations = () => {
   });
 
   Users.hasMany(Posts, common({ foreignKey: 'user_id' }));
+
+  Users.hasMany(Users, { foreignKey: 'supervisor_id', as: 'employees' });
+  Users.belongsTo(Users, { foreignKey: 'supervisor_id', as: 'supervisor' });
+
   Users.belongsToMany(Types, {
     through: 'users_types',
     foreignKey: 'user_id',
