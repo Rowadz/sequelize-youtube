86c684b95a13e30505a044b9ca8ca129bf13e63f
Rowad <38977667+MohammedAl-Rowad@users.noreply.github.com>
2020-04-05 00:12:16 +0300

    Create users.sql
    🐒

---
+++ src/sq/users.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (first_name,about,email,"role",created_at,updated_at,supervisor_id) VALUES 
+('rowad','about rowad','example@test.com','user','2020-03-31 22:21:01.068','2020-03-31 22:21:01.068',NULL)
+,('sarah','about sarah','example2@test.com','user','2020-03-31 22:21:14.866','2020-03-31 22:21:14.866',NULL)
+,('polo','about plo','polo@test.com','user','2020-03-31 22:21:26.081','2020-03-31 22:21:26.081',NULL)
+,('name','about name','name@test.com','user','2020-03-31 22:21:36.022','2020-03-31 22:21:36.022',3)
+;
