e8715920154aba203dc4450092a11122135ccdd6
Rowad <38977667+MohammedAl-Rowad@users.noreply.github.com>
2020-04-05 00:09:53 +0300

    Update index.js
    🐒 lazy 🐒

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ const Users = sequelize.import(__dirname + '/models/users.model.js');
 const Posts = sequelize.import(__dirname + '/models/posts.model.js');
 const Types = sequelize.import(__dirname + '/models/types.model.js');
 const UsersTypes = sequelize.import(__dirname + '/models/users_types.model.js');
+const UsersFollowers = sequelize.import(
+  __dirname + '/models/users_followers.model.js'
+);
+
+const { writeFileSync } = require('fs');
 
 sequelize
   .authenticate()
@@ -12,28 +17,29 @@ sequelize
     // await Posts.sync({ force: true });
     // await Types.sync({ force: true });
     // await UsersTypes.sync({ force: true });
-    await sequelize.sync({ force: true });
+    await sequelize.sync({ force: false });
     const res = await Users.findAll({
-      include: ['supervisor', 'employees'],
-      logging: console.log
+      include: ['supervisor', 'employees', 'followers', 'following'],
+      logging: console.log,
     });
-    console.log(
+    writeFileSync(
+      'users.json',
       JSON.stringify(
-        res.map(el => el.get({ plain: true })),
+        res.map((el) => el.get({ plain: true })),
         null,
         2
       )
     );
   })
-  .catch(err => {
+  .catch((err) => {
     console.error(err);
   });
 
 const defineRelations = () => {
-  const common = options => ({
+  const common = (options) => ({
     ...options,
     onDelete: 'CASCADE',
-    onUpdate: 'CASCADE'
+    onUpdate: 'CASCADE',
   });
 
   Users.hasMany(Posts, common({ foreignKey: 'user_id' }));
@@ -41,18 +47,46 @@ const defineRelations = () => {
   Users.hasMany(Users, { foreignKey: 'supervisor_id', as: 'employees' });
   Users.belongsTo(Users, { foreignKey: 'supervisor_id', as: 'supervisor' });
 
-  Users.belongsToMany(Types, {
-    through: 'users_types',
-    foreignKey: 'user_id',
-    otherKey: 'type_id'
-  });
-  Types.belongsToMany(Users, {
-    through: 'users_types',
-    foreignKey: 'type_id',
-    otherKey: 'user_id'
-  });
-  UsersTypes.belongsTo(Types, common({ foreignKey: 'type_id' }));
-  UsersTypes.belongsTo(Users, common({ foreignKey: 'user_id' }));
+  Users.belongsToMany(
+    Users,
+    common({
+      through: 'users_followers',
+      foreignKey: 'follower_id',
+      otherKey: 'following_id',
+      as: 'followers',
+    })
+  );
+
+  Users.belongsToMany(
+    Users,
+    common({
+      through: 'users_followers',
+      foreignKey: 'following_id',
+      otherKey: 'follower_id',
+      as: 'following',
+    })
+  );
+
+  Users.belongsToMany(
+    Types,
+    common({
+      through: 'users_types',
+      foreignKey: 'user_id',
+      otherKey: 'type_id',
+    })
+  );
+
+  Types.belongsToMany(
+    Users,
+    common({
+      through: 'users_types',
+      foreignKey: 'type_id',
+      otherKey: 'user_id',
+    })
+  );
+
+  UsersTypes.belongsTo(Types, { foreignKey: 'type_id' });
+  UsersTypes.belongsTo(Users, { foreignKey: 'user_id' });
   Users.hasMany(UsersTypes, common({ foreignKey: 'user_id' }));
   Types.hasMany(UsersTypes, common({ foreignKey: 'type_id' }));
 };
